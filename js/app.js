e7e0b6bc7754fa105074148c33de0d650873abee
Blake Wang <blakecwang@gmail.com>
2015-01-28 13:08:24 -0800

    program running, collision working

---
+++ js/app.js
@@ -0,0 +1,163 @@
+// overall dimension control
+var blockUnit = 100;
+var blockWidth = 5;
+var blockHeight = 6;
+var canvasWidth = blockWidth*blockUnit;
+var canvasHeight = blockHeight*blockUnit;
+
+// score keeper
+var score = 0;
+
+// image controls
+var waterImgSrc = 'images/square-blue.png';
+var roadImgSrc = 'images/square-black.png';
+var grassImgSrc = 'images/square-yellow.png';
+var enemyImgSrc = 'images/van.png';
+var playerImgSrc = 'images/frog.png';
+
+
+// enemy controls
+var enemyCount = 10;
+var enemyX;;
+var enemyY;
+var enemySpeed;
+enemyInit = function() {
+    enemyX = -(canvasWidth + blockUnit) + (Math.random() * canvasWidth);
+    enemyY = blockUnit * (Math.floor((Math.random() * 3) + 1));
+    enemySpeed = Math.random() * 100 + 100;
+};
+
+
+// player controls
+var playerX;
+var playerY;
+var playerIncrement = blockUnit;
+playerInit = function() {
+    playerX = (canvasWidth / 2) - (blockUnit / 2);
+    playerY = blockUnit * 4;
+};
+
+
+// initialize enemy objects
+var Enemy = function() {
+    enemyInit();
+    this.sprite = enemyImgSrc;
+    this.x = enemyX;
+    this.y = enemyY;
+    this.speed = enemySpeed;
+};
+
+
+// updates enemy position
+Enemy.prototype.update = function(dt) {
+
+    this.x += this.speed * dt;
+    
+    if (this.x > canvasWidth) {
+        enemyInit();
+        this.x = enemyX;
+        this.y = enemyY;
+        this.speed = enemySpeed;
+    }
+
+    //handle collisions
+    var enemyCenterX = this.x + blockUnit / 2;
+    var enemyCenterY = this.y + blockUnit / 2;
+    var playerCenterX = player.x + blockUnit / 2;
+    var playerCenterY = player.y + blockUnit / 2;
+    var distance = Math.sqrt(Math.pow((enemyCenterX - playerCenterX), 2)
+                 + Math.pow((enemyCenterY - playerCenterY), 2));
+    if (distance <= blockUnit / 2) {
+
+        playerInit();
+        player.x = playerX;
+        player.y = playerY;
+
+        enemyInit();
+        this.x = enemyX;
+        this.y = enemyY;
+        this.speed = enemySpeed;
+
+        score = 0;
+    }
+};
+
+
+// render enemy objects to screen
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// initialize player objects
+var Player = function() {
+    playerInit();
+    this.sprite = playerImgSrc;
+    this.x = playerX;
+    this.y = playerY;
+};
+
+
+// update player position
+Player.prototype.update = function() {
+    switch (this.keyPressed) {
+    case 'left':
+        if (this.x > 0) {
+            this.x -= playerIncrement;
+        }
+        break;
+    case 'up':
+        if (this.y > 0) {
+            this.y -= playerIncrement;
+        }
+        break;
+    case 'right':
+        if (this.x < canvasWidth - blockUnit) {
+            this.x += playerIncrement;
+        }
+        break;
+    case 'down':
+        if (this.y < canvasHeight - blockUnit) {
+            this.y += playerIncrement;
+        }
+        break;
+    }
+    this.keyPressed = '';
+
+    // if (this.y === 0) {
+
+    // }
+};
+
+
+// render player to screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// handle keys sent by the event listener
+Player.prototype.handleInput = function(key) {
+    console.info( key + ", ");
+    this.keyPressed = key;
+};
+
+
+// instantiate objects
+var player = new Player();
+var allEnemies = [];
+for (var i=0; i<enemyCount; i++) {
+    allEnemies.push(new Enemy());
+}
+
+
+// listens for key presses
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
